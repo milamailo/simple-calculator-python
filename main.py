60fae9c5ac0a6bbc926bbcd08f238f49f9652485
milamailo <miila.maiilo@gmail.com>
2024-07-12 01:12:32 -0400

    menu added to main file

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from src.cli_dashboards import cli_menu
 from src.gui_dashboard import gui_menu
+
 def main():
-    # cli_menu()
-    gui_menu()
+    while True:
+        choice = input("Choose your interface or exit:\n1. CLI\n2. GUI\n0. Exit\nEnter 1, 2, or 0: ")
+        if choice == '1':
+            cli_menu()
+        elif choice == '2':
+            gui_menu()
+        elif choice == '0':
+            print("Exiting the application.")
+            break  # Breaks the loop and exits the program
+        else:
+            print("Invalid input, please enter 1, 2, or 0.")
+
 if __name__ == "__main__":
     main()
